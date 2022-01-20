6c20a6893595f4dc275b2bea70ecc04c965d954e
NikolayPasyuk <kolyan-1812@mail.ru>
2022-01-20 16:05:24 +0300

    Case

---
+++ 08.js
@@ -0,0 +1,9 @@
+// Variable declarations
+var stuldLyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+stuldLyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
