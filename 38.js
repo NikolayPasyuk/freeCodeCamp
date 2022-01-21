6867642f1ab0e9cfbe4b9629937b130e420240b0
NikolayPasyuk <kolyan-1812@mail.ru>
2022-01-21 14:50:31 +0300

    const myNoun = dog;

---
+++ 38.js
@@ -0,0 +1,18 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =
+    "My " +
+    myNoun +
+    " is" +
+    " " +
+    myAdjective +
+    " but it" +
+    " " +
+    myVerb +
+    " " +
+    myAdverb; // Change this line
+// Only change code above this line
